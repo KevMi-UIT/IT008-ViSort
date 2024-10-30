a68629815dc9129bf0d5b733ff607d9e3dd76735
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-10-30 15:08:38 +0700

    feat(db): add db for user user service
    also add sort utils

---
+++ ViSort/Database/UserService.cs
@@ -0,0 +1,47 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ViSort.Models;
+
+namespace ViSort.Database
+{
+    internal class UserService
+    {
+        private readonly IMongoCollection<UserModel> UsersCollection;
+
+        public UserService()
+        {
+            var client = new MongoClient(DatabaseConstants.ConnectionString);
+            var database = client.GetDatabase(DatabaseConstants.Database);
+            UsersCollection = database.GetCollection<UserModel>(DatabaseConstants.UsersCollection);
+        }
+
+        public async Task SaveOrUpdateUser(UserModel user)
+        {
+            var filter = Builders<UserModel>.Filter.Eq(u => u.Username, user.Username);
+            var existingUser = await UsersCollection.Find(filter).FirstOrDefaultAsync();
+            if (existingUser != null)
+            {
+                if (UserModel.VerifyPassword(user.Password, existingUser.Password))
+                {
+                    var update = Builders<UserModel>.Update.Set(u => u.Score, user.Score);
+                    await UsersCollection.UpdateOneAsync(filter, update);
+                }
+                else
+                {
+                    throw new System.UnauthorizedAccessException("Password does not match.");
+                }
+            }
+            else
+            {
+                await UsersCollection.InsertOneAsync(user);
+            }
+        }
+
+        public async Task<List<UserModel>> GetAllUsersResult()
+        {
+            var projection = Builders<UserModel>.Projection.Include(u => u.Username).Include(u => u.Score);
+            return await UsersCollection.Find(_ => true).Project<UserModel>(projection).SortByDescending(u => u.Score).ToListAsync();
+        }
+    }
+}
---
+++ ViSort/Models/UserModel.cs
@@ -0,0 +1,112 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ViSort.Models
+{
+    internal class UserModel
+    {
+        [BsonId]
+        internal ObjectId Id { get; set; }
+
+        [BsonElement("username")]
+        internal string Username { get; private set; }
+
+        [BsonElement("password")]
+        internal string EncryptedPassword { get; private set; }
+
+        internal string Password
+        {
+            get
+            {
+                return Password;
+            }
+            private set
+            {
+                Password = value;
+                EncryptedPassword = EncryptPassword(value);
+            }
+        }
+
+        [BsonElement("score")]
+        internal int Score { get; private set; }
+
+        internal UserModel(string username, string password, int score)
+        {
+            Username = username;
+            Password = password;
+            Score = score;
+        }
+
+        internal static string EncryptPassword(string unencryptedPassword)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = GetHash(unencryptedPassword); // Create the AES key using the password's hash
+                aes.GenerateIV();
+                byte[] iv = aes.IV;
+
+                // Encrypt the password
+                using (ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, iv))
+                {
+                    byte[] passwordBytes = Encoding.UTF8.GetBytes(unencryptedPassword);
+                    byte[] encryptedPassword = encryptor.TransformFinalBlock(passwordBytes, 0, passwordBytes.Length);
+
+                    // Combine IV and encrypted password for storage
+                    byte[] result = new byte[iv.Length + encryptedPassword.Length];
+                    Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
+                    Buffer.BlockCopy(encryptedPassword, 0, result, iv.Length, encryptedPassword.Length);
+
+                    return Convert.ToBase64String(result);
+                }
+            }
+        }
+
+        internal static bool VerifyPassword(string inputPassword, string storedEncryptedPassword)
+        {
+            try
+            {
+                // Convert the base64 encrypted password back to byte array
+                byte[] fullCipher = Convert.FromBase64String(storedEncryptedPassword);
+
+                // Extract IV from stored password
+                byte[] iv = new byte[16];
+                Buffer.BlockCopy(fullCipher, 0, iv, 0, iv.Length);
+
+                // Extract encrypted password bytes
+                byte[] encryptedPassword = new byte[fullCipher.Length - iv.Length];
+                Buffer.BlockCopy(fullCipher, iv.Length, encryptedPassword, 0, encryptedPassword.Length);
+
+                // Decrypt password
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = GetHash(inputPassword); // Generate key from input password
+                    aes.IV = iv;
+
+                    using (ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                    {
+                        byte[] decryptedPasswordBytes = decryptor.TransformFinalBlock(encryptedPassword, 0, encryptedPassword.Length);
+                        string decryptedPassword = Encoding.UTF8.GetString(decryptedPasswordBytes);
+
+                        return inputPassword == decryptedPassword;
+                    }
+                }
+            }
+            catch
+            {
+                return false; // If decryption fails, password does not match
+            }
+        }
+
+        // Generates a 256-bit hash key from the password
+        private static byte[] GetHash(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
