d0466e472b359a4120493dc49febefd248936a68
Leminionn <minhlephan1901@gmail.com>
2024-10-31 01:59:26 +0700

    chore: fix conflict and add multiple choice

--- ViSort/GameForm/GameWindow.xaml.cs
+++ ViSort/GameForm/GameWindow.xaml.cs
@@ -16,17 +16,36 @@ using System.Windows.Shapes;
 namespace ViSort.GameForm
 {
     /// <summary>
-    /// Interaction logic for Window1.xaml
+    /// Interaction logic for GameWindow.xaml
     /// </summary>
     public partial class GameWindow : Window
     {
         public GameWindow()
         {
             InitializeComponent();
+            LoadAndShuffleQuestions();
             DisplayCurrentQuestion();
         }
         private List<string> QuestionList = new List<string>();
         private int currentQuestionIndex = 0;
+
+        private void LoadAndShuffleQuestions()
+        {
+            var filePath = "GameForm/question.txt";
+
+            // Đọc tất cả câu hỏi từ tệp
+            var questions = File.ReadAllLines(filePath).ToList();
+
+            // Xáo trộn danh sách câu hỏi
+            var random = new Random();
+            questions = questions.OrderBy(x => random.Next()).ToList();
+
+            // Thêm câu hỏi vào WrapPanel
+            foreach (var question in questions)
+            {
+                QuestionList.Add(question);
+            }
+        }
 
         private void DisplayCurrentQuestion()
         {
@@ -47,11 +66,25 @@ namespace ViSort.GameForm
 
         private void NextQuestion(object sender, RoutedEventArgs e)
         {
-            if (currentQuestionIndex >= 0 && currentQuestionIndex < QuestionList.Count)
+            if (currentQuestionIndex >= 0 && currentQuestionIndex < QuestionList.Count - 1)
             {
                 currentQuestionIndex++;
                 DisplayCurrentQuestion();
             }
         }
+
+        private void Answer_Checked(object sender, RoutedEventArgs e)
+        {
+            // Xác định nút radio nào đã được chọn
+            var selectedRadioButton = sender as RadioButton;
+
+            if (selectedRadioButton != null)
+            {
+                string selectedAnswer = selectedRadioButton.Content.ToString();
+                // Thực hiện hành động với câu trả lời đã chọn
+                MessageBox.Show($"Bạn đã chọn: {selectedAnswer}");
+            }
+        }
+
     }
 }
