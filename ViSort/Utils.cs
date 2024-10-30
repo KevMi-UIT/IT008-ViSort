a0194ef9bb78c865887bddfb700a77946256334c
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-10-30 15:52:07 +0700

    feat: add user util and calc score util

---
+++ ViSort/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ViSort
+{
+    internal static class Utils
+    {
+        internal static readonly int MaxScore = 999;
+        internal static readonly double ScoreSensitive = 0.01;
+        internal static int CalcScore(int steps)
+        {
+            return Convert.ToInt32(MaxScore / (1 + steps * ScoreSensitive));
+        }
+    }
+}
