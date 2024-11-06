4728716c7888c50b1924b7c59703a0a3abc8da96
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-11-06 20:57:35 +0700

    chore: change namespace style

--- ViSort/Database/DatabaseConstants.cs
+++ ViSort/Database/DatabaseConstants.cs
@@ -1,9 +1,8 @@
-﻿namespace ViSort.Database
+﻿namespace ViSort.Database;
+
+internal static class DatabaseConstants
 {
-    internal static class DatabaseConstants
-    {
-        internal static readonly string ConnectionString = @"mongodb+srv://visort:visort@visort.00edy.mongodb.net/?retryWrites=true&w=majority&appName=ViSort";
-        internal static readonly string Database = "ViSort";
-        internal static readonly string UsersCollection = "users";
-    }
+    internal static readonly string ConnectionString = @"mongodb+srv://visort:visort@visort.00edy.mongodb.net/?retryWrites=true&w=majority&appName=ViSort";
+    internal static readonly string Database = "ViSort";
+    internal static readonly string UsersCollection = "users";
 }
--- ViSort/Database/UserService.cs
+++ ViSort/Database/UserService.cs
@@ -4,69 +4,68 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using ViSort.Models;
 
-namespace ViSort.Database
+namespace ViSort.Database;
+
+internal class UserService
 {
-    internal class UserService
+    private readonly IMongoCollection<UserModel> UsersCollection;
+
+    internal UserService()
     {
-        private readonly IMongoCollection<UserModel> UsersCollection;
+        var client = new MongoClient(DatabaseConstants.ConnectionString);
+        var database = client.GetDatabase(DatabaseConstants.Database);
+        UsersCollection = database.GetCollection<UserModel>(DatabaseConstants.UsersCollection);
+    }
 
-        internal UserService()
+    private static void CheckUser(UserModel user, UserModel userDB)
+    {
+        if (userDB == null)
         {
-            var client = new MongoClient(DatabaseConstants.ConnectionString);
-            var database = client.GetDatabase(DatabaseConstants.Database);
-            UsersCollection = database.GetCollection<UserModel>(DatabaseConstants.UsersCollection);
+            throw new UnauthorizedAccessException("User not found.");
         }
+        if (user.EncryptedPassword != userDB.EncryptedPassword)
+        {
+            throw new UnauthorizedAccessException("Password does not match.");
+        }
+    }
 
-        private static void CheckUser(UserModel user, UserModel userDB)
-        {
-            if (userDB == null)
-            {
-                throw new UnauthorizedAccessException("User not found.");
-            }
-            if (user.EncryptedPassword != userDB.EncryptedPassword)
-            {
-                throw new UnauthorizedAccessException("Password does not match.");
-            }
-        }
+    private async Task AddUserAsync(UserModel user)
+    {
+        await UsersCollection.InsertOneAsync(user);
+    }
 
-        private async Task AddUserAsync(UserModel user)
-        {
-            await UsersCollection.InsertOneAsync(user);
-        }
+    internal async Task<List<UserModel>> GetAllUsersResult()
+    {
+        var projection = Builders<UserModel>.Projection.Include(u => u.Username).Include(u => u.Score != 0);
+        return await UsersCollection.Find(_ => true).Project<UserModel>(projection).SortByDescending(u => u.Score).ToListAsync();
+    }
 
-        internal async Task<List<UserModel>> GetAllUsersResult()
+    internal async Task<bool> AuthUserAsync(UserModel user)
+    {
+        var filter = Builders<UserModel>.Filter.Eq(u => u.Username, user.Username);
+        var existingUser = await UsersCollection.Find(filter).FirstOrDefaultAsync();
+        if (existingUser == null)
         {
-            var projection = Builders<UserModel>.Projection.Include(u => u.Username).Include(u => u.Score != 0);
-            return await UsersCollection.Find(_ => true).Project<UserModel>(projection).SortByDescending(u => u.Score).ToListAsync();
+            await AddUserAsync(user);
+            return true;
         }
+        return user.EncryptedPassword == existingUser.EncryptedPassword;
+    }
 
-        internal async Task<bool> AuthUserAsync(UserModel user)
-        {
-            var filter = Builders<UserModel>.Filter.Eq(u => u.Username, user.Username);
-            var existingUser = await UsersCollection.Find(filter).FirstOrDefaultAsync();
-            if (existingUser == null)
-            {
-                await AddUserAsync(user);
-                return true;
-            }
-            return user.EncryptedPassword == existingUser.EncryptedPassword;
-        }
+    internal async Task UpdateScore(UserModel user)
+    {
+        var filter = Builders<UserModel>.Filter.Eq(u => u.Username, user.Username);
+        var existingUser = await UsersCollection.Find(filter).FirstOrDefaultAsync();
+        CheckUser(user, existingUser);
+        var update = Builders<UserModel>.Update.Set(u => u.Score, user.Score);
+        await UsersCollection.UpdateOneAsync(filter, update);
+    }
 
-        internal async Task UpdateScore(UserModel user)
-        {
-            var filter = Builders<UserModel>.Filter.Eq(u => u.Username, user.Username);
-            var existingUser = await UsersCollection.Find(filter).FirstOrDefaultAsync();
-            CheckUser(user, existingUser);
-            var update = Builders<UserModel>.Update.Set(u => u.Score, user.Score);
-            await UsersCollection.UpdateOneAsync(filter, update);
-        }
-
-        internal async void DeleteUserAsync(UserModel user)
-        {
-            var filter = Builders<UserModel>.Filter.Eq(u => u.Username, user.Username);
-            var existingUser = await UsersCollection.Find(filter).FirstOrDefaultAsync();
-            CheckUser(user, existingUser);
-            UsersCollection.DeleteOne(filter);
-        }
+    internal async void DeleteUserAsync(UserModel user)
+    {
+        var filter = Builders<UserModel>.Filter.Eq(u => u.Username, user.Username);
+        var existingUser = await UsersCollection.Find(filter).FirstOrDefaultAsync();
+        CheckUser(user, existingUser);
+        UsersCollection.DeleteOne(filter);
     }
 }
