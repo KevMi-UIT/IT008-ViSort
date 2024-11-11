7ff89e148ec32de3dd427b34f5888fbdfabdb8e7
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-11-11 09:04:58 +0700

    feat(db): add custom exceptions

---
+++ ViSort/Database/UserExceptions.cs
@@ -0,0 +1,13 @@
+﻿namespace ViSort.Database;
+
+internal static class UserExceptions
+{
+
+    internal class PasswordDoesNotMatch(string message) : Exception(message)
+    {
+    }
+
+    internal class UserNotFound(string message) : Exception(message)
+    {
+    }
+}
