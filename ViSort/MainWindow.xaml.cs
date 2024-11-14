f4a342831c41384a2abeec9c81d313024b5aedcf
Phan Lê Minh <137601647+Leminionn@users.noreply.github.com>
2024-11-14 09:46:10 +0700

    feat: add main UI, quiz UI, scoreboard (#2)
    * first commit in this branch
    
    * feat: create simple interface with some sample questions.
    
    * chore: rm conflict
    
    * chore: fix conflict and add multiple choice
    
    * merge branch "main" to "game"
    
    * refactor: change form's name, change List<string>question to Class question.
    
    * feat: create authentication basic form
    
    * feat: upgradge to dotnet 8.0 from leminionn
    
    * feat: remove button_selection when change question
    
    * refactor(quiz): WIP
    
    * fix(deps): dup deps WPFUI
    
    * chore: WIP
    
    * chore(deps): update deps
    
    * feat: create scoreboard, login_button
    
    * feat: create logout button, check validate authentication before submit
    
    * chore: format before merge
    
    * Update ViSort/MainWindow.xaml.cs
    
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>
    
    * Update ViSort/MainWindow.xaml.cs
    
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>
    
    * Update ViSort/UserFillForm/UserFillForm.xaml.cs
    
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>
    
    * Update ViSort/QuizForm/QuizWindow.xaml.cs
    
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>
    
    * fix: close only auth window , not make whole app
    
    * fix: authenticate user with try-catch; allow cancel auth window ( messagebox ok cancel)
    
    * chore: rename element following convention rules
    
    * chore: remove question.txt
    
    ---------
    
    Co-authored-by: KevinNitroG <trannguyenthaibinh46@gmail.com>
    Co-authored-by: codefactor-io[bot] <47775046+codefactor-io[bot]@users.noreply.github.com>

--- ViSort/MainWindow.xaml.cs
+++ ViSort/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿namespace ViSort;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-namespace ViSort;
+using Windows.System;
+using Wpf.Ui.Controls;
+using MessageBox = System.Windows.MessageBox;
+using MessageBoxButton = System.Windows.MessageBoxButton;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
@@ -23,5 +27,54 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OpenQuizWindow(object sender, RoutedEventArgs e)
+    {
+        var QuestFilePath = "QuizWindow/QuizQuestionsList.cs";
+        if (!File.Exists(QuestFilePath))
+        {
+            MessageBox.Show("Không tìm thấy tệp câu hỏi. \nVui lòng kiểm tra lại đường dẫn", "Lỗi nè", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+
+        if (App.User == null)
+        {
+            var UserFillForm = new UserFillForm();
+            UserFillForm.ShowDialog();
+        }
+
+        var QuizWindow = new QuizWindow.QuizWindow();
+        QuizWindow.ShowDialog();
+    }
+
+    private async void CheckUserLogin_Click(object sender, RoutedEventArgs e)
+    {
+        if (App.User == null)
+        {
+            var loginWindow = new UserFillForm();
+            loginWindow.ShowDialog();
+
+            if (App.User != null)
+            {
+                await App.UserSvc!.AuthUserAsync(App.User!);
+                Login_Icon.Symbol = SymbolRegular.PeopleCheckmark24;
+                LogoutButton.Visibility = Visibility.Visible;
+            }
+        }
+    }
+    private void ScoreBoard_Click(object sender, RoutedEventArgs e)
+    {
+        var ScoreBoard = new ScoreBoard.ScoreBoard();
+        ScoreBoard.ShowDialog();
+    }
+    private void CheckUserLogout_Click(object sender, RoutedEventArgs e)
+    {
+        if (App.User != null)
+        {
+            App.User = null;
+            Login_Icon.Symbol = SymbolRegular.ArrowEnter20;
+            LogoutButton.Visibility = Visibility.Hidden;
+        }
     }
 }
---
+++ ViSort/UserFillForm/UserFillForm.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ViSort.Models;
+using Windows.System;
+using static ViSort.Database.UserExceptions;
+
+
+namespace ViSort;
+
+/// <summary>
+/// Interaction logic for UserFillForm.xaml
+/// </summary>
+public partial class UserFillForm : Window
+{
+    public UserFillForm()
+    {
+        InitializeComponent();
+        App.UserSvc ??= new();
+    }
+
+    private async void SubmitButton_Click(object sender, RoutedEventArgs e)
+    {
+        string password = Password_Passwordbox.Password;
+        UserModel User = new(UsernameTextbox.Text, password);
+
+        if (!UserUtils.ValidateUsername(User.Username))
+        {
+            ValidateUsernameTextBlock.Visibility = Visibility.Visible;
+            return;
+        }
+        ValidateUsernameTextBlock.Visibility = Visibility.Hidden;
+
+        if (!UserUtils.ValidatePassword(password))
+        {
+            ValidatePasswordTextBlock.Visibility = Visibility.Visible;
+            return;
+        }
+        ValidatePasswordTextBlock.Visibility = Visibility.Hidden;
+        SubmitButton.IsEnabled = false;
+
+        try
+        {
+            await App.UserSvc!.AuthUserAsync(User);
+            App.User = User;
+        }
+        catch (PasswordDoesNotMatch)
+        {
+            MessageBox.Show("Mật khẩu không chính xác.", "Lỗi đăng nhập", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        catch (UserNotFound)
+        {
+            MessageBox.Show("Người dùng không tồn tại.", "Lỗi đăng nhập", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+        finally
+        {
+            SubmitButton.IsEnabled = true;
+            if (App.User == null)
+            {
+                MessageBox.Show("Đăng nhập không thành công.", "Lỗi đăng nhập", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+    }
+
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(UsernameTextbox.Text) && string.IsNullOrWhiteSpace(Password_Passwordbox.Password))
+        {
+            var result = MessageBox.Show("Chưa có thông tin đăng nhập. Tiếp tục mà không đăng nhập?",
+                                         "Warning", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
