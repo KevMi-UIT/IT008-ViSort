8ae80070890f8ba6b89a74958a40166f55ea687e
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-11-18 15:53:28 +0700

    fix: forget to add from migration Exceptions

--- ViSort/UserFillForm/UserFillForm.xaml.cs
+++ ViSort/UserFillForm/UserFillForm.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using ViSort.Models;
 using Windows.System;
-using static ViSort.Database.UserExceptions;
+using static ViSort.Exceptions.UserExceptions;
 
 namespace ViSort;
 
