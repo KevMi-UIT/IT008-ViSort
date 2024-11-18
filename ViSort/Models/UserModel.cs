d295277973765b4cf0d3222ef94188143f6756a3
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-11-18 15:47:29 +0700

    refactor(db): change the way hash password, split db to services & exceptions
    still upload unhashed pass to db

--- ViSort/Models/UserModel.cs
+++ ViSort/Models/UserModel.cs
@@ -2,6 +2,7 @@
 using MongoDB.Bson.Serialization.Attributes;
 using System.Security.Cryptography;
 using System.Text;
+using ViSort.Utils;
 
 namespace ViSort.Models;
 
@@ -14,7 +15,7 @@ public class UserModel
     public string Username { get; private set; }
 
     [BsonElement("password")]
-    public string EncryptedPassword { get; private set; }
+    public string HashedPassword { get; private set; }
 
     private string _password;
     public string Password
@@ -23,7 +24,7 @@ public class UserModel
         private set
         {
             _password = value;
-            EncryptedPassword = EncryptPassword(value);
+            HashedPassword = Hasher.Hash(value);
         }
     }
 
@@ -34,7 +35,7 @@ public class UserModel
     {
         Username = username;
         _password = password;
-        EncryptedPassword = EncryptPassword(password);
+        HashedPassword = Hasher.Hash(password);
         Score = score;
     }
 
@@ -48,61 +49,17 @@ public class UserModel
         Score = score;
     }
 
-    public static string EncryptPassword(string unencryptedPassword)
+    public override bool Equals(object? obj)
     {
-        using Aes aes = Aes.Create();
-        aes.Key = GetHash(unencryptedPassword); // Create the AES key using the password's hash
-        aes.GenerateIV();
-        byte[] iv = aes.IV;
-
-        // Encrypt the password
-        using ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, iv);
-        byte[] passwordBytes = Encoding.UTF8.GetBytes(unencryptedPassword);
-        byte[] encryptedPassword = encryptor.TransformFinalBlock(passwordBytes, 0, passwordBytes.Length);
-
-        // Combine IV and encrypted password for storage
-        byte[] result = new byte[iv.Length + encryptedPassword.Length];
-        Buffer.BlockCopy(iv, 0, result, 0, Buffer.ByteLength(iv));
-        Buffer.BlockCopy(encryptedPassword, 0, result, iv.Length, Buffer.ByteLength(encryptedPassword));
-
-        return Convert.ToBase64String(result);
+        if (obj is UserModel user)
+        {
+            return user.Username == Username && user.Password == Password;
+        }
+        return false;
     }
 
-    public static bool VerifyPassword(string inputPassword, string storedEncryptedPassword)
+    public override int GetHashCode()
     {
-        try
-        {
-            // Convert the base64 encrypted password back to byte array
-            byte[] fullCipher = Convert.FromBase64String(storedEncryptedPassword);
-
-            // Extract IV from stored password
-            byte[] iv = new byte[16];
-            Buffer.BlockCopy(fullCipher, 0, iv, 0, Buffer.ByteLength(iv));
-
-            // Extract encrypted password bytes
-            byte[] encryptedPassword = new byte[fullCipher.Length - iv.Length];
-            Buffer.BlockCopy(fullCipher, iv.Length, encryptedPassword, 0, Buffer.ByteLength(encryptedPassword));
-
-            // Decrypt password
-            using Aes aes = Aes.Create();
-            aes.Key = GetHash(inputPassword); // Generate key from input password
-            aes.IV = iv;
-
-            using ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-            byte[] decryptedPasswordBytes = decryptor.TransformFinalBlock(encryptedPassword, 0, encryptedPassword.Length);
-            string decryptedPassword = Encoding.UTF8.GetString(decryptedPasswordBytes);
-
-            return inputPassword == decryptedPassword;
-        }
-        catch
-        {
-            return false; // If decryption fails, password does not match
-        }
-    }
-
-    // Generates a 256-bit hash key from the password
-    private static byte[] GetHash(string password)
-    {
-        return SHA256.HashData(Encoding.UTF8.GetBytes(password));
+        throw new NotImplementedException();
     }
 }
